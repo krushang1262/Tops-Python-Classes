35d26d10fb9549cb0fe939ee7e1c2d86a36593b6
Balajiwala krushang <kdb.sd20@gmail.com>
2025-12-23 10:46:01 +0530

    all

---
+++ Classwork/datatype.py
@@ -0,0 +1,36 @@
+a = 10
+print(type(a))
+
+salary = 125.654
+print(type(salary))
+
+complex = 5j+1j
+print(type(complex))
+
+inpt = int(input("Enter a number: "))
+print("value is the ", inpt)
+print(type(inpt))
+
+lst = [1,2,3,4,5]
+print(lst)
+print(type(lst))
+
+tp = (10,20,30,40,50,60)
+print(tp)
+print(type(tp))
+
+dct = {"name":"krushang","sname":"balaji"}
+print(dct)
+print(type(dct))
+
+setsss = {5,4,3,2,1}
+print(setsss)
+print(type(setsss))
+
+b = True
+print(b)
+print(type(b))
+
+nm = input("Enter name:")
+age = 23
+print(f"your name is: {nm} and age is: {age}")
---
+++ Classwork/operators.py
@@ -0,0 +1,73 @@
+# arithmetic opt
+
+a= 10 
+b = 20
+
+print(a +b)
+print(a -b)
+print(a *b)
+print(a /b)
+print(a ** b)
+print(a // b)
+print("abc"+"xyz")
+print("abc"+str(a))
+
+print("***************************")
+
+# assignment opt
+
+a = 10
+
+a+=10
+a-=5
+a*=7
+a/=9
+a//=8
+a**=6
+print(a)
+print("***************************")
+# comparision opt
+
+a= 10
+b = 20
+c = 30
+print("1" ,a==b)
+print("2" ,a!=b)
+print("3" ,a>b)
+print("4" ,c>b)
+print("5" ,a>=b)
+print("6" ,a<=b)
+print("***************************")
+# logical opt
+
+print(True and False)
+print(True and True)
+print(False and False)
+print(False and True)
+
+print(False or True)
+print(False or False)
+print(True or True)
+print(True or False)
+print(not True)
+print("***************************")
+# identity opt
+
+a = [10, 20]
+b = [10,20]
+
+print(a is b)
+print(a is not b)
+print(a == b)
+print("***************************")
+# membership opt
+
+a = [10,20,30]
+b = "xyz"
+
+print(10 in a)
+print(40 not in a)
+print('x' in b)
+print('y' not in b)
+
+print("***************************")
