14bbe74a8f20981bad4fc0f489f0ec359f6023ce
krushangtops <krushangtops@gmail.com>
2025-12-26 12:32:41 +0530

    code changes in the conditional file

--- Classwork/Conditional.py
+++ Classwork/Conditional.py
@@ -1,47 +1,49 @@
 marks = int(input("Enter your Marks: "))
 
-if marks >=91 and marks<=100:
+if marks>=90 and marks <=100:
+    print("Grade A++")
+elif marks>=85 and marks <=89:
     print("Grade A")
-elif marks >= 71 and marks <=90:
+elif marks>=75 and marks <=84:
+    print("Gade B+")
+elif marks >=69 and marks <=74:
     print("Grade B")
-elif marks >=51 and marks <=70:
+elif marks >=50 and marks <=59:
     print("Grade C")
-elif marks >= 50 and marks <=35:
+elif marks >=40 and marks <=49:
     print("Grade D")
-elif marks <=34:
-    print("Fail")
-else:
-    print("Invalid output")
+elif marks <40:
+    print("You are Failed!")
 
 # print("********************match catch********************************")
 
-choice =  int(input("Enter your choice"))
-match choice:
-    case 1:
-        print("gujarati")
-    case 2:
-        print("hindi")
-    case 3:
-        print("english")
-    case 4:
-        print("marathi")
-    case _:
-        print("Invalid choice")
+# choice =  int(input("Enter your choice"))
+# match choice:
+#     case 1:
+#         print("gujarati")
+#     case 2:
+#         print("hindi")
+#     case 3:
+#         print("english")
+#     case 4:
+#         print("marathi")
+#     case _:
+#         print("Invalid choice")
 
 # print("********************match catch*****calc***************************")
 
-num1 = int(input("Enter number 1: "))
-operation = input("Added a Calculation method ")
-num2 = int(input("Enter number 2: "))
+# num1 = int(input("Enter number 1: "))
+# operation = input("Added a Calculation method ")
+# num2 = int(input("Enter number 2: "))
 
-match operation:
-    case "add":
-        print("Added value is",num1+num2)
-    case "min":
-        print("minus value is",num1-num2)
-    case "mult":
-        print("multiply value is",num1*num2)
-    case "div":
-        print("division value is",num1/num2)
-    case _:
-        print("Invalid opeation your perform")
+# match operation:
+#     case "add":
+#         print("Added value is",num1+num2)
+#     case "min":
+#         print("minus value is",num1-num2)
+#     case "mult":
+#         print("multiply value is",num1*num2)
+#     case "div":
+#         print("division value is",num1/num2)
+#     case _:
+#         print("Invalid opeation your perform")
