66ad5bf0d38700edc5122c508bfbea3327573db4
krushangtops <krushangtops@gmail.com>
2025-12-29 11:27:15 +0530

    implement the prime number code

--- Classwork/Task.py
+++ Classwork/Task.py
@@ -1,13 +1,27 @@
-number = int(input("Enter a number: "))
+# number = int(input("Enter a number: "))
 
-for i in range(1,11):
-    tab = number * i
-    print(number, '*',i,'=', tab)
-print("*****************table****************************")
+# for i in range(1,11):
+#     tab = number * i
+#     print(number, '*',i,'=', tab)
+# print("*****************table****************************")
+
+# for i in range(1,101):
+#     if i%2==0:
+#         print(i, "is Even")
+#     else:
+#         print(i,"is Odd")
+# print("****************odd/even*****************************")
 
 for i in range(1,101):
-    if i%2==0:
-        print(i, "is Even")
+    rem =0
+    for j in range(2,i):
+        if i%j==0:
+            rem=1
+            break
+    
+    if rem ==0:
+        print(i,"is prime no.")
     else:
-        print(i,"is Odd")
-print("****************odd/even*****************************")
+        print(i,"is not prime no.")
+
+print("****************prime number *****************************")
