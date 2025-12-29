c4c3196244db705ffc32496b75c77365295e8ec4
krushangtops <krushangtops@gmail.com>
2025-12-29 11:29:50 +0530

    all

--- Classwork/Task.py
+++ Classwork/Task.py
@@ -1,16 +1,16 @@
-# number = int(input("Enter a number: "))
+number = int(input("Enter a number: "))
 
-# for i in range(1,11):
-#     tab = number * i
-#     print(number, '*',i,'=', tab)
-# print("*****************table****************************")
+for i in range(1,11):
+    tab = number * i
+    print(number, '*',i,'=', tab)
+print("*****************table****************************")
 
-# for i in range(1,101):
-#     if i%2==0:
-#         print(i, "is Even")
-#     else:
-#         print(i,"is Odd")
-# print("****************odd/even*****************************")
+for i in range(1,101):
+    if i%2==0:
+        print(i, "is Even")
+    else:
+        print(i,"is Odd")
+print("****************odd/even*****************************")
 
 for i in range(1,101):
     rem =0
