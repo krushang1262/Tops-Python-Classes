2635fc382b8286bda0620560faeff1401f01efa9
krushangtops <krushangtops@gmail.com>
2026-01-25 14:59:21 +0530

    all

--- Assignment/63.py
+++ Assignment/63.py
@@ -1,1 +1,11 @@
 # 63) Write a Python function to check whether a number is perfect or not
+
+def perf_num(number):
+    divisor = sum([num for num in range(1,number) if number % num == 0])
+    
+    if number == divisor:
+        print(f"{number} is perfect number")
+    else:
+        print(f"{number} is not perfect number")
+    
+perf_num(6)
