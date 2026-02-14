ae4e810a985be58939e1f9708ac38000d98c5dbc
krushangtops <krushangtops@gmail.com>
2026-02-14 13:22:20 +0530

    implement the regex

---
+++ Classwork/Regex.py
@@ -0,0 +1,70 @@
+import re
+
+# k = re.match("K","Krushng Balajiwala")
+# print(k)
+
+# k = re.findall("Balaji","Balajiwala Krushang")
+# print(k)
+
+# k = re.finditer("o","Good morning")
+# print(next(k))
+# print(next(k))
+# print(next(k))
+
+# k = re.search("^H","Hello Python is great")
+# print(k)
+
+# k = re.search("gold","kolar gold field where gold are produce more. in mines")
+# print(k)
+
+# k = re.sub("g","F","krushang is a good boy")
+# print(k)
+
+# k = re.split(" ","Krushang Balajiwala")
+# print(k)
+
+# print("=========================================ADV==========================================================")
+
+# fa = re.findall("K.u","Krushang Balajiwala")
+# print(fa)
+
+# s = re.search("tops$","I am from tops technology tops")
+# print(s)
+
+# s1 = re.search("go+d", "i am from gooooooood condition")
+# print(s1)
+
+# s2 = re.search("he*l","heeeeeeeeello world, hello python, great")
+# print(s2)
+
+# s3 = re.search("hk?o","hkkko python sir")
+# print(s3)
+
+# k = re.findall("[a-z0-9A-Z]","HELlo python guyz i am audible now 123456")
+# print(k)
+
+# k = re.findall(r"\d","Hello python from tops @ 123")
+# print(k)
+
+# k = re.findall(r"\D","Hello python from Tops @ ")
+# print(k)
+
+# k = re.findall(r"\w","hello from tops 123 @")
+# print(k)
+
+# k = re.findall(r"\W","hello from tops 123 @")
+# print(k)
+
+# k = re.findall(r"\S","hello from tops 123 @")
+# print(k)
+
+# txt = "this is the certificate issued"
+# k = re.search(r"\Bued", txt)
+# print(k)
+
+# tx = re.search(r"\d{5,10}","7896541230")
+# print(tx)
+
+# num = "7896541230"
+# ph = re.match("^[0-9]{10}$", num)
+# print(ph)
