e297e314364c23c6d728ea90206f139e810eb9c9
krushangtops <krushangtops@gmail.com>
2026-03-23 12:37:21 +0530

    all

---
+++ Libraries/Numpy/operator.py
@@ -0,0 +1,34 @@
+# import numpy as np
+
+x = 10
+y = 20
+
+if x == y:
+    print("yes")
+else:
+    print("no")
+    
+if x != y:
+    print("yes")
+else:
+    print("no")
+    
+if x > y:
+    print("yes")
+else:
+    print("no")
+    
+if x < y:
+    print("yes")
+else:
+    print("no")
+    
+if x >= y:
+    print("yes")
+else:
+    print("no")
+    
+if x <= y:
+    print("yes")
+else:
+    print("no")
