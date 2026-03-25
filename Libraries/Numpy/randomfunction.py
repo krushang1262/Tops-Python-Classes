5975ababa2cd82ede77afd552f878c35f4a92257
krushangtops <krushangtops@gmail.com>
2026-03-25 20:15:17 +0530

    all

--- Libraries/Numpy/randomfunction.py
+++ Libraries/Numpy/randomfunction.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-
-print(np.random.rand)
-print(np.random.randint) 
-print(np.random.choice) 
-print(np.random.seed) 
+print(np.random.rand(3))
+print(np.random.randint(1, 10))
+print(np.random.choice([10,20,30,40], size=3))
+print(np.random.rand())
+np.random.seed(1)
---
+++ Libraries/Numpy/transpose.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+a = np.array([
+    [
+        [1,2,3],
+        [4,5,6]
+    ],
+    [
+        [10,20,30],
+        [40,50,60]
+    ]
+])
+
+print(np.transpose(a))
