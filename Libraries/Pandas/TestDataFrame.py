3b0b744cd7abd35b82fec654b6d7bdf5edc596d6
krushangtops <krushangtops@gmail.com>
2026-03-26 11:00:38 +0530

    uncmt

--- Libraries/Pandas/TestDataFrame.py
+++ Libraries/Pandas/TestDataFrame.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-# df = pd.DataFrame({'Name': ['Ajay','Sunil','Jatin'], 'age': [20,21,10],'contact': [9913141301, 9173789108, 8469558093]})
-# print(df)
-# df.to_json("~/Desktop/DataSheet/myData.json")
+df = pd.DataFrame({'Name': ['Ajay','Sunil','Jatin'], 'age': [20,21,10],'contact': [9913141301, 9173789108, 8469558093]})
+print(df)
+df.to_json("~/Desktop/DataSheet/myData.json")
 
 # print("===========================================================================================================================")
 
-# dfcsv = pd.read_csv("~/Desktop/DataSheet/diamonds.csv")
-# print(dfcsv)
+dfcsv = pd.read_csv("~/Desktop/DataSheet/diamonds.csv")
+print(dfcsv)
 
 # print("===========================================================================================================================")
 
